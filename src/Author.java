ffd27e13cbfef27b7c859f6f8a25fc8fe2e38dee
artur <artursuper10@gmail.com>
2022-11-17 20:38:30 +0300

    MethodHw

--- src/Author.java
+++ src/Author.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Author {
     private final String name;
     private final String surname;
@@ -10,8 +12,25 @@ public class Author {
 
     public String getSurname() { return this.surname; }
 
-    public String getFullName() {
+    @Override
+    public String toString() {
         return this.name + " " + this.surname;
+    }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Author author = (Author) o;
+        return Objects.equals(name, author.name) && Objects.equals(surname, author.surname);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, surname);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,7 @@ public class Main {
         Author stephenKing = new Author("stephen", "king");
         Author levTolstoy = new Author("Lev", "Tolstoy");
         Book theStand = new Book("The Stand", stephenKing, 1975);
-        System.out.println(" Название книги: " + theStand.getTitle() + " Автор:" + theStand.getAuthor().getFullName()
-        + " Дата:" + theStand.getPublicationYear());
+
 
     }
 }
